6ff0b4257545a70f8641cfef0f8cb9e3a604db8b
Sagar Mehta <55478536+sagarhere4u@users.noreply.github.com>
2025-12-19 14:41:56 +0530

    Added Null Resource

--- main.tf
+++ main.tf
@@ -5,3 +5,9 @@ resource "local_file" "foo" {
   content  = "Current Environment: ${var.env}."
   filename = "env"
 }
+
+resource "null_resource" "example" {
+   provisioner "local-exec" {
+     command = "hostname ; whoami ; date ; sleep 100 ; echo done"
+   }
+}
